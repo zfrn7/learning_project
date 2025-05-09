a955f0cb43fa6e10e5d8585b7439200a06aef674
Zafer Örün <zaferorunn@gmail.com>
2025-05-09 18:23:45 +0300

    Sıcaklık ve sayı kontrol kodu eklendi

---
+++ learning_project.py
@@ -0,0 +1,27 @@
+"""
+not1 = int(input(" Lütfen Notu Giriniz: "))
+not2 = int(input("Lütfen Notu Giriniz: "))
+not3 = int(input("lütfen Notu Giriniz: "))
+toplam = (not1 + not2 + not3 )
+ortalama = (toplam / 3)
+print(round(ortalama))
+"""
+"""
+sıcaklık = float(input("Sıcaklık derecesini giriniz: "))
+
+if sıcaklık < 36.1:
+    print("Sıcaklık çok düşük")
+elif sıcaklık < 37.2:
+    print("Normal")
+elif sıcaklık < 38:
+    print("Hafif ateş var")
+elif sıcaklık < 40:
+    print("Ateş var")
+else:
+    print("Çok yüksek ateş")
+"""
+tam_sayı = int(input("Bir Tam Sayı Seçiniz"))
+if tam_sayı > 20 and (tam_sayı % 2 == 1) :
+    print("girdiğiniz sayı 20den büyük tek sayıdır")
+else :
+    print("girdiğiniz sayı istenilen değeri karşılamıyor")
---
+++ using_if_and_else.py
@@ -0,0 +1,5 @@
+sıcaklık = float(input("lütfen vücut sıcaklığını gir: "))
+if sıcaklık > 37:
+    print("DİKKAT YÜKSEK ATEŞ!!")
+else:
+    print("HER ŞEY NORMAL")
